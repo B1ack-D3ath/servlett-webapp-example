f1d5e0dbb17636d3cca4747871225a12243fe3e8
B1ack-D3ath <128817169+B1ack-D3ath@users.noreply.github.com>
2024-05-26 10:59:54 +0300

    Kydet Servlet Example

--- src/main/java/KaydetServlet.java
+++ src/main/java/KaydetServlet.java
@@ -4,19 +4,22 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class KaydetServlet extends HttpServlet {
+
+    PrintWriter out = null;
+    String responseHeaderTanimi = "<html><head>"
+        + "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>"
+        + "<title>Kayıt Sayfası</title>"
+        + "</head>";
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         // Istemcinin response ile donecek verinin tipini bilip ona göre verinin dogru
         //gosterilmesi saglaniyor
         response.setContentType("text/html;charset=UTF-8");
         // Response nesnesine icerik yazmak icin PrintWriter nesnesi olusturuluyor
-        PrintWriter out = null;
-        String responseHeaderTanimi = "<html><head>";
-        responseHeaderTanimi += "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>";
-        responseHeaderTanimi += "<title>Kayıt Sayfası</title>";
-        responseHeaderTanimi += "</head>";
         try {
             out = response.getWriter();
             out.println(responseHeaderTanimi);
@@ -42,6 +45,24 @@ public class KaydetServlet extends HttpServlet {
             out.println("<p>Soyisim: " + soyisim + "</p>");
             out.println("<p>Cinsiyet: " + strCinsiyet + "</p>");
             out.println("<p>Sınıf: " + sinif + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } finally {
+            if (out != null)
+                out.close(); // Yazici nesne kapatiliyor
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            out = response.getWriter();
+            out.println(responseHeaderTanimi);
+            out.println("<body>");
+            out.println("<h1>Girilen Bilgiler</h1>");
+            out.println("<p>Anana Girmiştir....:D:D:D:D:D:D:D</p>");
             out.println("</body>");
             out.println("</html>");
         } catch (IOException e) {
